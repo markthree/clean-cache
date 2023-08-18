b599207416a5b7feefecdaac97ba7e60019de6e8
markthree <1801982702@qq.com>
2023-08-18 13:21:24 +0800

    feat: add help

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/fatih/color"
+)
+
+func isHelp() bool {
+	for _, v := range os.Args {
+		if v == "-h" || v == "--help" || v == "help" {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
+
+	if isHelp() {
+		fmt.Print("\n")
+		color.Cyan("clean-cache")
+		fmt.Print("Description: go 写的清理 node 项目缓存，超级无敌快\n\n")
+		os.Exit(0)
+	}
 
 }
